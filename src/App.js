8597a23086a5f35722e89fd73279561c7220b458
frankagathos <frantzeskosagathos@gmail.com>
2019-01-12 22:11:50 +0000

    added new buttons,styling for dev

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ this.state = {
    } 
 this.onSearchChange = this.onSearchChange.bind(this);
 this.onCountryClick = this.onCountryClick.bind(this);
+this.getNextImage = this.getNextImage.bind(this);
 }
 
     
@@ -70,13 +71,15 @@ this.onCountryClick = this.onCountryClick.bind(this);
             selectedCountrypopulation:selectedCountry[0].population,
             results_style:'user-has-chosen',
             country_stats_style:'show',
+            photocounter:1,
+         
         });
        
        
         //Get photos of selected Country using UNSPLASH API
         const query= selectedCountry[0].name;
-        const client_id ="8ed09088a4eb4d257e69127e636984bbf65599f0f00bc9a10a35d759d3f2b7d2"  ;
-        let photocounter = this.state.photocounter ; 
+        const client_id ="8ed09088a4eb4d257e69127e636984bbf65599f0f00bc9a10a35d759d3f2b7d2";
+        let photocounter = 0; 
           
         //API request  of users country
         fetch(`https://api.unsplash.com/search/photos?client_id=${client_id}&query=${query}`)
@@ -87,40 +90,69 @@ this.onCountryClick = this.onCountryClick.bind(this);
             this.setState({          
                 numberOfPhotos:json.results.length,
                 city_photo_url:json.results[photocounter].urls.regular,
-                photocounter:this.state.photocounter+1,
-              
+                all_city_urls:json.results,
+               
             }     
             )
-        })
-   
-         console.log('number of photos:'+this.state.numberOfPhotos);
+        })      
 }
 
 getNextImage = () => {
-        //Get NEXT PHOTO of selected Country using UNSPLASH API
-        const query= this.state.selectedCountryName;
-        const client_id ="8ed09088a4eb4d257e69127e636984bbf65599f0f00bc9a10a35d759d3f2b7d2"  ;
-        let photocounter = this.state.photocounter ; 
-          
-        //API request  of users country
-        fetch(`https://api.unsplash.com/search/photos?client_id=${client_id}&query=${query}`)
-         .then(res => res.json())
-         .then(json => {
-            
-
+  
+        let photocounter = this.state.photocounter; 
+        const TotalPhotos = this.state.numberOfPhotos;
+        const AllUrls = this.state.all_city_urls;
+    if (TotalPhotos>photocounter){     
             this.setState({          
-                city_photo_url:json.results[photocounter].urls.regular,
+                city_photo_url:AllUrls[photocounter].urls.regular,
                 photocounter:this.state.photocounter+1,
-              
-            }     
-            )
-        })
+            })
+            }
+ 
+     else{
+            this.setState({          
+                city_photo_url:AllUrls[0].urls.regular,
+                photocounter:1,
+            })
+            }
+                  
 }
-   
 
-
-    render () {
+getPreviousImage = () =>{
+    let photocounter = this.state.photocounter; 
+    const TotalPhotos = this.state.numberOfPhotos;
+    const AllUrls = this.state.all_city_urls;
+    console.log("photocounter="+photocounter);
+    let firstPrevious = true;
+    if (firstPrevious){
         
+        if(photocounter>1){
+         this.setState({          
+                city_photo_url:AllUrls[photocounter-2].urls.regular,
+                photocounter:this.state.photocounter-1,
+                })
+            firstPrevious=false;
+        }
+       else {
+                this.setState({          
+                city_photo_url:AllUrls[TotalPhotos-1].urls.regular,
+                photocounter:TotalPhotos,
+                })
+            firstPrevious=false;
+       }
+    } 
+  
+    else {
+                this.setState({          
+                city_photo_url:AllUrls[photocounter-1].urls.regular,
+                photocounter:this.state.photocounter-1,
+            })
+        
+          
+     
+    }    
+}
+    render () {
         //check if it'loading 
         if(this.state.isLoading){
                            return <p>Loading....</p>
@@ -136,18 +168,18 @@ getNextImage = () => {
                                                {this.state.allCountriesStats.filter(isSearched(this.state.searchTerm)).map(item=>
                             
                                                         <div className="country-card" key={item.alpha3Code} onClick={() => this.onCountryClick(item.alpha3Code)}>
-                                                           <div> {item.name} </div>
+                                                            <div> {item.name} </div>
                                                             <div className="region">{item.region}</div>
-                                                             <div> <div className="flag-wrapper"><img src={item.flag} alt={item.flag}/></div></div>
+                                                            <div> <div className="flag-wrapper"><img src={item.flag} alt={item.flag}/></div></div>
                                                                             
                                                        
                                                                            
                                                          </div>
                                                                                                        
                                                        ) }
-                         </div>
+                          </div>
                     </div>
-                    
+               
                     <div className={this.state.country_stats_style}>
                           
                            {this.state.selectedCountryName} cover(s) an area of  {this.state.selectedCountryArea}km².The capital is {this.state.selectedCountryCapital}.<br></br>
@@ -155,10 +187,15 @@ getNextImage = () => {
                            
                            <div className='photo-wrapper'>
                                      <img src={this.state.city_photo_url} alt={this.state.selectedCountryName}></img>
+                                     
                            </div>
-                     
-                           
-                           <button onClick={this.getNextImage}>NEXT IMAGE</button>
+                        <div>
+                                 {this.state.photocounter+'/'+this.state.numberOfPhotos}
+                        </div>
+                          
+                           <button onClick={this.getPreviousImage}>PREVIOUS</button>
+                           <button onClick={this.getNextImage}>NEXT</button>
+              
                           
                     </div>
          
